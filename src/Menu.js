853b4cdba44a4d392a0fd01eeec3691b6d6c3618
chunchuney@gmail.com <{chunchuney@gmail.com}>
2020-02-08 17:00:37 +0900

    부트스트랩 적용

--- src/Menu.js
+++ src/Menu.js
@@ -1,27 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import {
+    Collapse, DropdownItem, DropdownMenu,
+    DropdownToggle,
+    Nav,
+    Navbar,
+    NavbarToggler,
+    NavItem,
+    UncontrolledDropdown
+} from "reactstrap";
+import {NavLink} from "react-router-dom";
 
 export const Menu = (props) =>{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link" href="#">Features</a>
-                        <a className="nav-item nav-link" href="#">Pricing</a>
-                        <a className="nav-item nav-link disabled" href="#" tabIndex="-1"
-                           aria-disabled="true">Disabled</a>
-                    </div>
+            <Navbar color="dark" dark expand="md">
+                <div className="container">
+                    <NavLink to="/" className="navbar-brand">React</NavLink>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink to="/heroes" className="nav-link">Heroes</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink to="/scoreboard" className="nav-link">Scoreboard</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink to="/product" className="nav-link">Product</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Reset
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
                 </div>
-            </nav>
+            </Navbar>
         </div>
     )
 };
